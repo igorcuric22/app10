21c968f7fd5fc2ef901a53bea3bf61b2db6fa0cd
igorcuric22 <igor.curic55@gmail.com>
2023-12-22 17:40:36 +0100

    clean

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useState,useEffect} from "react";
 
-export default App;
+let id;
+
+const App = () => {
+  
+    const [actors,setActors]=useState("");
+    const [song,setSong]=useState("");
+    const [datax,setData]=useState();
+
+    const handleActors=(e)=>{
+        setActors(e.target.value);
+    }
+
+    const handleSong=(e)=>{
+        setSong(e.target.value);
+    }
+
+    const loadApi=()=>{
+        console.log(`${song} ${actors}`)
+
+        id=0;
+        setTimeout(()=>{
+        const url = `https://itunes.apple.com/search?term=${actors}&media=music&entity=${song}`;
+
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+            console.log(data.results);
+            setData(data.results);
+
+        
+        }
+        );
+
+    },2000)
+    
+    }
+  
+    useEffect(()=>{
+        if(actors.length===0) return;
+        console.log("Actors",actors);
+      },[actors])
+
+
+    return (
+      <div>
+        <h2>LIsta </h2>
+    
+    <div className="forma">
+    <label htmlFor="actors">Unesi ime</label>
+    <input type="text" id="actors" value={actors} onChange={handleActors}/>
+    </div>
+
+    <div className="forma">
+    <label htmlFor="song">Unesi zanr</label>
+    <input type="text" id="song" value={song} onChange={handleSong}/>
+    </div>
+    <div className="forma">
+    <button onClick={loadApi} >Stisni</button>
+    </div>
+    
+   {datax && 
+        (datax.length!==0?(
+            <div className="tablex">
+             <table className="tabla1">
+                <thead>
+                
+                </thead><tbody>{datax.map(item=>{
+            return(
+            <tr key={id++}>
+                <td>{item.artistName}</td>
+                <td>{item.collectionCensoredName}</td>
+                <td>{item.collectionName}</td>
+                <td>{item.collectionPrice}</td>
+            </tr>);
+        }
+
+        )}</tbody></table> </div>):(<div>Nema rezultata</div>))
+
+        }
+
+    
+ 
+  
+      </div>
+    );
+  };
+
+
+  export default App;
